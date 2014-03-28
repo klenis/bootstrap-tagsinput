06fa3787da53848e0832ad061d302bae5949dffd
klenis <klenis@klenis.org>
2014-03-28 10:39:09 +0100

    Static fix for placeholder due to broken Angular component

--- dist/bootstrap-tagsinput.js
+++ dist/bootstrap-tagsinput.js
@@ -31,7 +31,8 @@
     this.isSelect = (element.tagName === 'SELECT');
     this.multiple = (this.isSelect && element.hasAttribute('multiple'));
     this.objectItems = options && options.itemValue;
-    this.placeholderText = element.hasAttribute('placeholder') ? this.$element.attr('placeholder') : '';
+    /*this.placeholderText = element.hasAttribute('placeholder') ? this.$element.attr('placeholder') : '';*/
+    this.placeholderText = "Add tags";
     this.inputSize = Math.max(1, this.placeholderText.length);
 
     this.$container = $('<div class="bootstrap-tagsinput"></div>');
